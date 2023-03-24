d101623fe74e1dc1203238ecad981f654f7236c3
Жданова Екатерина Борисовна (4079982) <ebzhdanova@vtb.ru>
2023-03-24 08:40:44 +0700

    Тесты с allure + GitHub workflows

---
+++ src/test/java/allure/SelenideWithListenerTest.java
@@ -0,0 +1,28 @@
+package allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideWithListenerTest {
+
+    @Test
+    public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#80")).should(Condition.exist);
+    }
+}
